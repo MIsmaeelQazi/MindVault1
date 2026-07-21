96c6520a9dc560069227322c5ff1e48f92afe262
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-07-21 10:22:26 +0000

    Server Better

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,11 +1,11 @@
 from flask import Flask, send_from_directory
 import os
 
-Diary = Flask(__name__)
+app = Flask(__name__)
 
-@Diary.route("/")
+@app.route("/")
 def home():
     return send_from_directory(os.getcwd(), "index.html")
 
 if __name__ == "__main__":
-    Diary.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
